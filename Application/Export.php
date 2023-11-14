5074ec314c772f0d33fb1ade97c93b143c33987c
Yakov Permyakov <txncii@gmail.com>
2023-11-14 18:51:12 +0300

    improves generators sample

--- Application/Export.php
+++ Application/Export.php
@@ -8,17 +8,25 @@ use Generator;
 
 class Export
 {
-    // ...some constructor
+    // ...some code
 
     public function exportMetrics(Instance $instance, Category $category, bool $serialize = false): Generator
     {
+        $counter = 0;
         if ($serialize) {
             foreach ($this->getMetrics($instance, $category) as $metricItem) {
+                $counter++;
                 yield $this->serializer->serialize($metricItem, 'json');
             }
-        }else{
+        } else {
             yield from $this->getMetrics($instance, $category);
         }
+
+        return $counter;
     }
 
+    private function getMetrics(Instance $instance, Category $category): iterable
+    {
+        return $this->repository->read($instance, $category);
+    }
 }
